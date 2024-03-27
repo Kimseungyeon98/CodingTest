5a70a14c42ae81ccfa83c8d543b745eeb9b520c0
Kimseungyeon98 <121033246+Kimseungyeon98@users.noreply.github.com>
2024-03-27 19:00:35 +0900

    [Silver II] Title: 유기농 배추, Time: 156 ms, Memory: 15748 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -6,23 +6,22 @@ public class Main {
 	static int[][] arr;
 	static int cnt;
 	static int xMax,yMax,n;
-	static boolean[][] visit;
+	static boolean[][] visited;
 	static int[] dx = {0,-1,0,1};
 	static int[] dy = {1,0,-1,0};
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
-		
 		int t = Integer.parseInt(st.nextToken());
+		
 		for(int i=0; i<t ; i++) {
 			st = new StringTokenizer(br.readLine());
-			// 가로 길이
+			// 가로 최대 길이
 			xMax = Integer.parseInt(st.nextToken());
-			// 세로 길이
+			// 세로 최대 길이
 			yMax = Integer.parseInt(st.nextToken());
 			// 썩은 배추의 수
 			n = Integer.parseInt(st.nextToken());
@@ -31,7 +30,7 @@ public class Main {
 			// 지렁이 수
 			cnt = 0;
 			//방문여부
-			visit = new boolean[yMax][xMax];
+			visited = new boolean[yMax][xMax];
 			
 			// 지도에 썩은 배추 위치 넣기
 			for (int j = 0; j < n; j++) {
@@ -42,10 +41,10 @@ public class Main {
 			}
 			
 			// 배추 순찰
-			for (int j = 0; j < yMax; j++) {
-				for (int k = 0; k < xMax; k++) {
-					if(arr[j][k]==1 && !visit[j][k]) {
-						dfs(k,j);
+			for (int y = 0; y < yMax; y++) {
+				for (int x = 0; x < xMax; x++) {
+					if(arr[y][x]==1 && !visited[y][x]) {
+						dfs(x,y);
 						cnt++;
 					}
 				}
@@ -54,16 +53,14 @@ public class Main {
 		}
 	}
 	
-	
 	static void dfs(int x, int y) {
-		visit[y][x] = true;
-		
+		visited[y][x] = true;
 		for(int i=0; i<4 ; i++) {
-			int cx = x + dx[i];
-			int cy = y + dy[i];
-			if(cx >= 0 && cy >= 0 && cx < xMax && cy < yMax) {
-				if (!visit[cy][cx] && arr[cy][cx] == 1) {
-					dfs(cx, cy);
+			int move_x = x + dx[i];
+			int move_y = y + dy[i];
+			if(move_x >= 0 && move_y >= 0 && move_x < xMax && move_y < yMax) {
+				if (!visited[move_y][move_x] && arr[move_y][move_x] == 1) {
+					dfs(move_x, move_y);
 				}
 			}
 		}
