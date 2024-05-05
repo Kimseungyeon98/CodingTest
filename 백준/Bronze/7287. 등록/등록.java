fb0c0aab30bbff1816f4e99a5be5021e68b73517
Kimseungyeon98 <121033246+Kimseungyeon98@users.noreply.github.com>
2024-05-05 14:46:42 +0900

    [Bronze V] Title: 등록, Time: 112 ms, Memory: 13932 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		System.out.println(210);
+        System.out.println("tmddus3037");
+	}
+}
