5174fb4a5436c033538cea9a29edadd2d4df3d6e
김승연 <121033246+Kimseungyeon98@users.noreply.github.com>
2024-07-06 20:59:53 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 49.81 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> st = new Stack<>();
+        for(int i:arr){
+            if(st.isEmpty()||st.peek()!=i) st.push(i);
+        }
+        int n = st.size();
+        int[] answer = new int[n];
+        for(int i=n-1; i>=0; i--) {
+            answer[i]=st.pop();
+        }
+        return answer;
+    }
+}
