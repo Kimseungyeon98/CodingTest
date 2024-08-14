46b8e7507010a1335fba951e2f8bc46fa3f81f73
김승연 <121033246+Kimseungyeon98@users.noreply.github.com>
2024-08-14 14:24:46 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        for(int i=1; i<10; i++){
+            answer+=i;
+        }
+        for(int n:numbers) answer-=n;
+        return answer;
+    }
+}
