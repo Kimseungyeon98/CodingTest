4f52e59c8979ee91b2e11899141de9d3681abd2d
김승연 <121033246+Kimseungyeon98@users.noreply.github.com>
2024-11-20 16:54:45 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((long)Math.pow(sc.nextInt(),2));
+        System.out.println(2);
+    }
+}
