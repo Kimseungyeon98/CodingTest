44d52883968d0b5952df0bc0e5eea8c26ffa8c63
김승연 <121033246+Kimseungyeon98@users.noreply.github.com>
2025-02-04 22:55:09 +0900

    [level 0] Title: 2의 영역, Time: 2.73 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -9,7 +9,7 @@ class Solution {
                 start = Math.min(start, i);
                 end = Math.max(end, i);
             }
-        }
+        } 
         if(start<=end){
             return Arrays.copyOfRange(arr,start,end+1);
         } else {
