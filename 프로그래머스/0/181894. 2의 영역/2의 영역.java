99ca0aa7f743bcca94aebfec1657e38bd52c08eb
김승연 <121033246+Kimseungyeon98@users.noreply.github.com>
2025-02-04 22:54:40 +0900

    Update 2의 영역.java

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -2,23 +2,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
         int[] answer = {};
-        int start = -1;
-        int end = -1;
-        int cnt = 0;
+        int start = (int)Integer.MAX_VALUE;
+        int end = 0;
         for(int i=0; i<arr.length; i++){
             if(arr[i]==2){
-                cnt++;
-                if(start==-1){
-                    start = i;
-                } else {
-                    end = i;
-                }
+                start = Math.min(start, i);
+                end = Math.max(end, i);
             }
         }
-        if(start!=-1&& end!=-1){
+        if(start<=end){
             return Arrays.copyOfRange(arr,start,end+1);
-        } else if (cnt==1){
-            return new int[]{2};
         } else {
             return new int[]{-1};
         }
