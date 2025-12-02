90b1faff14fc56822bfb566d742e0779377db834
김승연 <121033246+Kimseungyeon98@users.noreply.github.com>
2025-12-02 22:20:52 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.03 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[][] dots) {
+        int answer = 0;
+        int x=0,y = 0;
+        for(int i=0; i<3; i++){
+            x = Math.max(Math.abs(dots[i][0]-dots[i+1][0]),x);
+            y = Math.max(Math.abs(dots[i][1]-dots[i+1][1]),y);
+        }
+        return x*y;
+    }
+}
